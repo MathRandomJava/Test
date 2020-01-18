dd93e9bc25ecd1de6007c9bffad8fb59e041a009
Trickster <dmc.trickster@gmail.com>
2020-01-18 11:35:00 +0500

    add project

--- Parser/src/com/main/Main.java
+++ Parser/src/com/main/Main.java
@@ -1,6 +1,9 @@
 package com.main;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class Main {
 
@@ -15,56 +18,77 @@ public class Main {
         dictionary.add("brother");
 
         String sentence = "jesslookedjustliketimherbrother";
+        reSpace(sentence, dictionary);
 
-        System.out.println(bestSplit(dictionary, sentence));
     }
 
-    private static String bestSplit(HashSet<String> dictionary, String sentence) {
-        ParseResult[] memo = new ParseResult[sentence.length()];
-        ParseResult result = split(dictionary, sentence, 0, memo);
-        return result.parsed;
-    }
-
-    private static ParseResult split(HashSet<String> dictionary, String sentence, int start, ParseResult[] memo) {
-        if (start >= sentence.length()) {
-            return new ParseResult(0, "");
-        }
-
-        if (memo[start] != null) {
-            return memo[start];
-        }
-
-        int bestInvalid = Integer.MAX_VALUE;
-        String bestParsing = null;
-        String partial = "";
-        for (int i = start; i < sentence.length(); i++) {
-            char c = sentence.charAt(i);
-            partial += c;
-            int invalid = dictionary.contains(partial) ? 0 : partial.length();
-            if (invalid < bestInvalid) {
-                ParseResult result = split(dictionary, sentence, i+1, memo);
-                if (invalid + result.invalid < bestInvalid) {
-                    bestInvalid = invalid + result.invalid;
-                    bestParsing = partial + " " + result.parsed;
-                    if (bestInvalid == 0) {
-                        break;
-                    }
-                }
+    private static void reSpace(String input, HashSet<String> dictionary) {
+        System.out.println("---------------------------------------");
+        System.out.println("Входные параметры: " + input);
+        System.out.println("Словарь: " + dictionary);
+        Trie trie = new Trie(dictionary);
+        char[] chars = input.toCharArray();
+        Map<Character, Letter> letters = trie.letters;
+        Letter lastLetter = null;
+        StringBuilder sb = new StringBuilder();
+        int unrecognizedCharsCount = 0;
+        for (int i = 0; i < chars.length; i++) {
+            int j = 0;
+            while (i + j < chars.length && letters.containsKey(chars[i + j])) {
+                lastLetter = letters.get(chars[i + j]);
+                letters = lastLetter.children;
+                j++;
             }
+
+            if (lastLetter != null && lastLetter.wordEnd) {
+                if (sb.length() > 0 && sb.charAt(sb.length() - 1) != ' ') {
+                    sb.append(' ');
+                }
+                sb.append(new String(Arrays.copyOfRange(chars, i, i + j)));
+                sb.append(' ');
+                i += (j - 1);
+            } else {
+                sb.append(chars[i]);
+                unrecognizedCharsCount++;
+            }
+            lastLetter = null;
+            letters = trie.letters;
         }
 
-        memo[start] = new ParseResult(bestInvalid, bestParsing);
-        return memo[start];
+        System.out.println("Вывод: " + sb);
+        System.out.println("Нераспознанных символов: " + unrecognizedCharsCount);
     }
 
-    public static class ParseResult {
-        int invalid;
-        String parsed;
+    private static class Trie {
+        private Map<Character, Letter> letters = new HashMap<>();
 
-        ParseResult(int invalid, String parsed) {
-            this.invalid = invalid;
-            this.parsed = parsed;
+        Trie(HashSet<String> dictionary) {
+            for (String word : dictionary) {
+                Map<Character, Letter> candidates = letters;
+                char[] charArray = word.toCharArray();
+                for (int i = 0; i < charArray.length; i++) {
+                    char c = charArray[i];
+                    Letter l = new Letter(i == charArray.length - 1);
+                    if (!candidates.containsKey(c)) {
+                        candidates.put(c, l);
+                        candidates = l.children;
+                    } else {
+                        candidates = candidates.get(c).children;
+                    }
+
+                }
+
+            }
+
         }
     }
 
+    private static class Letter {
+        private final boolean wordEnd;
+        private Map<Character, Letter> children = new HashMap<>();
+
+        Letter(boolean wordEnd) {
+            this.wordEnd = wordEnd;
+        }
+    }
 }
